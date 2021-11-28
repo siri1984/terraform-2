be0329374a127c232718e597a19d77b0c7b7edee
siri1984 <85675316+siri1984@users.noreply.github.com>
2021-11-28 22:11:54 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region     = "us-west-2"
- # access_key = "AKIAYQCQHYMMRZAAGCTC"
- # secret_key = "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"
+ # access_key = ""
+ # secret_key = "
+  "
 }
